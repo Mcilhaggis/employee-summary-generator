a6c3386ff76dcb8a846d2d27f42dca2dd7f92cb8
mcilhaggis <rachael.mcilhagga@live.co.uk>
2020-12-07 14:57:23 -0500

    adding inquirer questions

--- app.js
+++ app.js
@@ -10,11 +10,88 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+function teamQuestions(){
+inquirer
+    .prompt([
+                {
+            type: 'list',
+            messaage: 'What is your role?',
+            name: 'role',
+            choices: ['Manager', 'Engineer', 'Intern']
+        },{
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name'
+        },{
+            type: 'input',
+            message: 'What is your ID number?',
+            name: 'id'
+        },{
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email'
+        }, 
+        {
+            when: (response) => response.role === 'Manager',      
+            type:'input',
+            name: 'officeNumber',
+            message:'Please input your office number'
+        },
+        {
+            when: (response) => response.role === 'Engineer',
+            type:'input',
+            name: 'github',
+            message:'Please input in your github username:'
+        },
+        {
+            when: (response) => response.role === 'Intern',
+            type:'input',
+            name: 'school',
+            message:'Which school are you enrolled in?'
+        },
+        {
+            type: 'confirm',
+            message: 'Do you have more employees to enter?',
+            name: 'moreTeamMembers',
+            }, 
+    ]).then((response) => {
+        console.log(response);
+        employeeArr = [];
+        
+        if(response.role === 'Manager') {
+            const NewManager = new Manager(response.name. response.id, response.email, response.officeNumber);
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+            employeeArr.push(NewManager)
 
-// After the user has input all employees desired, call the `render` function (required
+        } else if(response.role === 'Engineer') {
+            const NewEngineer = new Engineer(response.name, response.id, response.email, response.github);
+
+            employeeArr.push(NewEngineer)
+
+        } else if(response.role === 'Intern') {
+            const NewIntern = new Intern(response.name. response.id, response.email, response.school)
+
+            employeeArr.push(NewIntern)
+        }
+
+        if(response.moreTeamMembers){ //this isn't looping the questions when i selecty manager or intern
+            teamQuestions();
+        
+        }else {
+            console.log(employeeArr);
+            render(employeeArr);//is this necessary?
+
+            fs.writeFile(outputPath, render(employeeArr), (err) =>
+            err ? console.log(err) : console.log('Success!')
+          );
+        }
+    }) 
+}
+
+teamQuestions();
+
+
+    // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
@@ -24,12 +101,4 @@ const render = require("./lib/htmlRenderer");
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
